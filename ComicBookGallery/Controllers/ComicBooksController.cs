688d4db89bdf00ed7a7fdf6016da34c875c1c0f1
Lucas Nordström <lucas_keno@hotmail.com>
2022-03-28 11:13:20 +0200

    Changed old controller into new

---
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers {
+    public class ComicBooksController : Controller {
+        public ActionResult Detail() {
+
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday) {
+
+                //return Redirect("/");
+
+                return Content("I've had it with this");
+               
+            }
+
+            return new ContentResult() {
+                Content = "Not monday"
+            };
+
+
+
+        }
+    }
+}
